73ef8df68fd74b0ea8b0cf91d380fd44f41dffe7
Jyotir Tyrese McEwanAnamoah <jyotirjy@gmail.com>
2022-10-31 15:53:48 -0400

    created and styled navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,14 +3,12 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h4>RELOCATE</h4>
+      <h4 className="coaster-rator-h4">Coaster Rator</h4>
       <div>
         <Link to="/" className="linky">
           Home
         </Link>
-        <Link to="" className="linky">
-          Ratings
-        </Link>
+        <Link to="" className="linky"></Link>
 
         <Link to=""></Link>
       </div>
