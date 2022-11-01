f37a2ade16c083542ded535d65ee0f6528177861
Jon Scarpelli <scarpelli.jon@gmail.com>
2022-11-01 13:20:04 -0500

    random coaster component

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -4,9 +4,11 @@ import { Link, useNavigate, Routes, Route } from "react-router-dom";
 import Results from "./Results";
 import Detail from "./Detail";
 import SideSearch from "./SideSearch";
+import RandomCoaster from "./RandomCoaster";
 
 const Main = () => {
   const [allCoasters, setAllCoasters] = useState(null);
+  const [chosenCoasters, setChosenCoasters] = useState(null);
   const [selectedCoaster, setSelectedCoaster] = useState(null);
 
   const getCoasters = async () => {
@@ -32,15 +34,14 @@ const Main = () => {
       <div className="SideSearch">
         <SideSearch />
       </div>
-      <h1>THIS IS THE MAIN AREA</h1>
       <Routes>
-        {/* <Route path="/" element={<Search />} /> */}
+        <Route path="/" element={<RandomCoaster coasters={allCoasters}/>} />
 
         <Route
           path="/results"
-          element={<Results onCLick={onClick} coasters={allCoasters} setCoasters={setAllCoasters} />}
-        />
-        <Route path="/coaster" element={<Detail coaster={selectedCoaster} />} />
+          element={<Results onCLick={onClick} coasters={allCoasters} setCoasters={setAllCoasters} />}/>
+        // {/* /> */}
+        // {/* <Route path="/coaster" element={<Detail coaster={selectedCoaster} />} /> */}
 
       </Routes>
     </div>
