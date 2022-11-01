e0bfa197360b86226c57c6553c7935579987f267
Derek Lewis <dlewis9442@gmail.com>
2022-10-31 21:44:37 -0500

    seed file complete

--- db/index.js
+++ db/index.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-let MONGODB_URI =
-  'mongodb+srv://coasterrator1:qwerty!@99@cluster0.xtgzrvn.mongodb.net/coaster'
+let MONGODB_URI = 'mongodb://127.0.0.1:27017/coasterrater'
+// 'mongodb+srv://coasterrator1:qwerty!@99@cluster0.xtgzrvn.mongodb.net/coaster'
 
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
-    console.log("Successfully connected to MongoDB.");
+    console.log('Successfully connected to MongoDB.')
   })
   .catch((e) => {
-    console.error("Connection error", e.message);
-  });
+    console.error('Connection error', e.message)
+  })
 
-const db = mongoose.connection;
+const db = mongoose.connection
 
-module.exports = db;
+module.exports = db
