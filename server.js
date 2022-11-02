3f46acfc6d6503a2955c13aa6eb959d2698fc692
eortiz65 <eortiz599@gmail.com>
2022-11-02 11:35:44 -0400

    preparation for heroku deployment

--- server.js
+++ server.js
@@ -9,10 +9,13 @@ const PORT = process.env.PORT || 3001
 const app = express()
 
 app.use(express.json())
-
+app.use(express.static(`${__dirname}/client/build`))
 app.use(logger('dev'))
 app.use(cors())
 app.use('/', routes)
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
